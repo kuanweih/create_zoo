5916375a3b900da9f025500eeae40bc1aba96b8e
Kw Huang <kwhuang@Kws-MacBook-Pro.wv.cc.cmu.edu>
2018-02-13 11:01:44 -0500

    fixed para con

--- writing_para_con-param.py
+++ writing_para_con-param.py
@@ -11,18 +11,18 @@ constrain_arg = args.constrain
 f = open('{0}/param_Constrained.param'.format(fastpm_path),'w')
 
 if constrain_arg == 'constrain':
-    f.write('InitCondFile                    {0}/../fastpm/results-za-constrain/fastpm_0.0100 \n'.format(fastpm_path))
+    f.write('InitCondFile                    ../fastpm/results-za-constrain/fastpm_0.0100 \n')
 else:
-    f.write('InitCondFile                    {0}/../fastpm/results-za/fastpm_0.0100 \n'.format(fastpm_path))
+    f.write('InitCondFile                    ../fastpm/results-za/fastpm_0.0100 \n')
 
-f.write('OutputDir                       {0} \n'.format(fastpm_path))
+f.write('OutputDir                       ../constrained \n')
 f.write('FileBase                        IC \n')
 f.write('Omega0                          0.2814          % Total matter density  (at z=0) \n')
 f.write('OmegaLambda                     0.7186          % Cosmological constant (at z=0) \n')
 f.write('OmegaBaryon                     0.0464          % Baryon density        (at z=0) \n')
 f.write('HubbleParam                     0.697           % Hubble paramater (may be used for power spec parameterization) \n')
 f.write('TimeBegin                       0.01            % scale factor \n')
-f.write('BoxSize                         20.0            % Mpc per side \n')
+f.write('BoxSize                         10.0            % Mpc per side \n')
 f.write('ProduceGas                      1               % 0:off, 1:on \n')
 f.write('MaxMemSizePerCore               1800 \n')
 f.write('NumPartPerFile                  131072 \n')
