545305e72ba10dae72611d1dce811ce05f4793f3
Kw Huang <kuanweih@andrew.cmu.edu>
2018-03-29 09:08:43 -0400

    create Con_x files

--- add_con_zoo.sh
+++ add_con_zoo.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 
-# Create main file named with the random seed (defined as a 6 digits string)
+# Target main file named with the random seed (defined as a 6 digits string)
 randomseed=000000
 boxsize=10  # Mpc/h
 n_particle=176  # Number of particles
-mkdir  run_"$boxsize"Mpc_$randomseed
+# mkdir  run_"$boxsize"Mpc_$randomseed TODO: remove if it works
+cons='Con_4  Con_5  Con_6'
 
 # Work under the main directory
 cd  run_"$boxsize"Mpc_$randomseed
 
 # Name files
-cons='Con_0  Con_1  Con_2  Con_3'
+# cons='Con_0  Con_1  Con_2  Con_3'
 ics_runs='ICs  Run_seed5e3  Run_seed5e4  Run_seed5e5'
 runs='Run_seed5e3  Run_seed5e4  Run_seed5e5'
 
 # Create files
 for i in $ics_runs; do
-    mkdir  $i
     for con in $cons; do
         mkdir  $i/$con
     done
 done
+# for i in $ics_runs; do
+#     mkdir  $i
+#     for con in $cons; do
+#         mkdir  $i/$con
+#     done
+# done
 
 # Create para and pbs files
 for con in $cons; do
